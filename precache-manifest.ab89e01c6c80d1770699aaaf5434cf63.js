0f813235585f1b3f251c5cbf871b81be9ac5cadf
Thomas <nguyen.thomastri@gmail.com>
2020-09-16 00:42:42 +1000

    Updates

---
+++ precache-manifest.ab89e01c6c80d1770699aaaf5434cf63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "464214744170259c48dd325b9dcb78e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "301848ac99f1999d36bf",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "2c6de2d1a9a83c22542e",
+    "url": "/static/css/main.c31210bd.chunk.css"
+  },
+  {
+    "revision": "301848ac99f1999d36bf",
+    "url": "/static/js/2.9f952e6d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9f952e6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c6de2d1a9a83c22542e",
+    "url": "/static/js/main.e0a6268a.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
