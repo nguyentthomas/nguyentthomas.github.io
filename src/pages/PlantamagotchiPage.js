002735cc1bd5209dcda00887349e19c2be5b7c29
Thomas Nguyen <nguyen.thomastri@gmail.com>
2020-09-24 23:31:29 +1000

    Added: Plantamagotchi Page + overall css

--- src/pages/PlantamagotchiPage.js
+++ src/pages/PlantamagotchiPage.js
@@ -32,7 +32,30 @@ function PlantamagotchiPage(props) {
 
                 <h5>Usability Testing / User Studies</h5>
                 <p>This project proved to ultimately provide me with experience where it was my first exposure to assisting in conducting user studies and usability tests that I would come to appreciate as an effective tool for designing using Human-centred design principles. 
-                     It also brought to my attention, physical design requirements and the accessibility that comes with it, to which plays a crucial role in the experience of users.</p>
+                     It also brought to my attention, physical design requirements and the accessibility that comes with it, to which plays a crucial role in the experience of users. <br/><br/>
+                     Through the collection of data collected through the user study, it was possible to gain further insights into the design with the inclusion of understanding the user needs gazing through their perspectives. The protocol of the user study, had a rather small sample size of 5, included enlisting a week-long experiment session, with a concluding short non-invasive 30 question interview, where the answers were transcribed immediately. The user study provided information on the prototype design, noting positive features as well as many features to improve upon. From the data, the findings can be categorised into the following common aspects, (being grouped to a relative concept). 
+                     
+                     With this particular usability test and user study, we interviewed a total of 8 participants where users were asked to initially use the device in particular use cases and was then asked a series of questions, guaging their reactions and answers in order to understand their feelings and gather valuable insights.
+                     <br/><br/>In particular, a series of insights was gained from user assessments, to which some of the pain points are:
+                     <ul>
+                        <li>Structure & Aesthetics: “It is not pleasing to the eye. It’s messy”. </li>
+                        <li>Usability & Quality of Life Improvements: 
+                            <ul>
+                                <li>“I don’t think it would be well accustomed to busy people, as I don’t see why they would clog up their schedule”</li>
+                                <li>“Make it as wireless as possible”</li>
+                                <li>“Screen could be made portable so that more monitoring can happen away from the plant” </li>
+                            </ul>
+                        </li>
+                        <li>User Engagement and Feedback (Visual and/or Audio): Two of the user study participants expressed their concern on how the entire device should be powered.</li>
+                     </ul>
+                     Recommendations were also made by users to which team members and I considered with our own input on the basis of improving the Plantamagotchi in the aspects mentioned prior.
+                     <ul>
+                        <li>For an enhanced experience, where the purpose of the prototype is to bring a more personal and engaging connection between the user and the plant, this is most definitely a glaring issue that must be rectified with future iterations. Alterations should involve an overarching theme, neatly packaging all of the internal components. This would allow it to appear more cohesive and as though it is not just hacked together. Suggestions also included references to concerns to wires and portability, where a solar-panel or powered solution would need to be worked into the design, as this would directly affect user's engagement.</li>
+                        <li>One suggestion was to include “Buttons to change displays – for measurements of different plant attributes” to improve the navigation and ease of use. Designing an experience with feedback like this in mind, will directly address the user’s needs. Some recommendations that were considered were increasing the size of the screen (specifically a 8x8 RGB LED Matrix to displays states of faces by having pre-programmed states that could enact when prompted by certain conditions.), or notifications displayed directly to the phone (though this could prove 'annoying' and frustrating for the user as to overwhelm them with constant updates) and another was providing navigation through a potentiometer to access 'pages' to the LCD screens whilst paired with the 8x8 LED to allow for more user input and control.</li>
+                        <li>The lack of engagement and audio feedback was a prominent issue amongst almost all of the user study participants. To address this, we will be implementing an array of pre-recorded lines to appeal to the user’s need of a social aspect, notifying them of instances when the plant is in need of assistance, rather than the users having to constantly monitor and look at the plant to maintain ideal conditions. The engagement levels needed to provide a more human-like interaction that was engaging. With one user suggesting that it should be able to talk and respond to what the user was saying, giving it a personality able to reply through dialogue. More interaction could be implemented in the form of mini games, or games that were tangential to the original purpose. Suggestions such as a leader board using a “competitive point system.”</li>
+
+                     </ul>
+                     </p>
 
                 <h5>Overview of Final Functional Prototype</h5>
                 <p>The Plantamagotchi is a physically interactive project that aimed to provide users a learning experience
